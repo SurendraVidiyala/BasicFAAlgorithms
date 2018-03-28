88cf957791c6a73160cbe6a68809d5e954fd38ee
surendra_vidiyala <vidiyala.surendra@gmail.com>
2018-03-28 19:40:01 -0400

    finding Duplicates in an array using bruteforce and hashset methods

---
+++ src/InterviewPrograms/Arrays/DuplicatesInArrayBruteForce.java
@@ -0,0 +1,28 @@
+package InterviewPrograms.Arrays;
+
+public class DuplicatesInArrayBruteForce {
+
+	/**
+	 * 
+	 * @author surendra_vidiyala 03/28/2017
+	 */
+	
+	/*
+	 * Java Program To Find Duplicate Elements In An Array Using Brute Force Method.
+	 * Using BruteForce Method, the time complexity is O(n^2).
+	 * 
+	 */
+	public static void main(String[] args) {
+
+		String[] arr = {"Surya","Arya","Navin","Suren","Surya","Madhu","Suren"};
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i+1; j < arr.length; j++) {
+				if((arr[i].equals(arr[j])) && (i!=j)) {
+					System.out.println("Duplicate Element is: "+arr[j]);
+				}
+			}
+		}
+	}
+
+}
---
+++ src/InterviewPrograms/Arrays/DuplicatesInArraysHashSet.java
@@ -0,0 +1,32 @@
+package InterviewPrograms.Arrays;
+
+import java.util.HashSet;
+
+public class DuplicatesInArraysHashSet {
+
+	/**
+	 * 
+	 * @author surendra_vidiyala 03/28/2017
+	 */
+
+	/*
+	 * Java Program To Find Duplicate Elements In An Array Using HashSet. Using
+	 * HashSet performance is better than BruteForce Method, the time complexity is
+	 * O(n).
+	 * 
+	 */
+	public static void main(String[] args) {
+
+		String[] strArr = { "Surya", "Arya", "Navin", "Suren", "Surya", "Madhu", "Suren" };
+
+		HashSet<String> set = new HashSet<>();
+
+		for (String arrElement : strArr) {
+			if (!set.add(arrElement)) {
+				System.out.println("Duplicate Element is : " + arrElement);
+			}
+		}
+
+	}
+
+}
