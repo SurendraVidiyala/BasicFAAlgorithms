f26375111cf12e124a4b4cd4269029f61c62b940
surendra_vidiyala <vidiyala.surendra@gmail.com>
2018-04-02 14:31:33 -0400

    Converting Integer to String in multiple methods, modified.

--- src/InterviewPrograms/Strings/IntegerToStringConversion.java
+++ src/InterviewPrograms/Strings/IntegerToStringConversion.java
@@ -7,6 +7,8 @@ public class IntegerToStringConversion {
 	 * @author surendra_vidiyala 04/02/2018
 	 */
 	/*
+	 * Program to convert Integer to String.
+	 * 
 	 * You are also often need to do the reverse conversion i.e converting from
 	 * integer to string. Java provides couple of methods to do that also. one is
 	 * Integer.toString() method and another one is String.valueOf() method. Both
@@ -15,15 +17,15 @@ public class IntegerToStringConversion {
 	public static void main(String[] args) {
 
 		int i = 2018;
-		
-        //Method: 1 Using toString() method.
+
+		// Method: 1 Using toString() method.
 		String str = Integer.toString(i);
-		
-		//Method: 2 Using valueOf() static methods.
+
+		// Method: 2 Using valueOf() static methods.
 		String value = String.valueOf(i);
-		
-		System.out.println("String value using static methods: "+value);
-		System.out.println("String value using toString() methood: "+str);
+
+		System.out.println("String value using static methods: " + value);
+		System.out.println("String value using toString() methood: " + str);
 	}
 
 }
