af8c56feb14b8d98bbc13ac6fc45a19591875892
surendra_vidiyala <vidiyala.surendra@gmail.com>
2018-04-02 12:47:58 -0400

    program to count the words in a string

---
+++ src/InterviewPrograms/Strings/CountTheWords.java
@@ -0,0 +1,53 @@
+package InterviewPrograms.Strings;
+
+import java.util.Scanner;
+
+public class CountTheWords {
+
+	/**
+	 * @author surendra_vidiyala 04/02/2018
+	 *
+	 */
+	/*
+	 * Java Program to count the number of words in a string.
+	 */
+
+	public static boolean stringCheck(String str) {
+		boolean status = true;
+		try {
+			if ((str == null) || (str == " ") || str.isEmpty()) {
+				// throw new RuntimeException();
+				status = false;
+			}
+		} catch (RuntimeException e) {
+			return !status;
+		}
+		return status;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Enter the String: ");
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+
+		// Method: 1
+		if (stringCheck(str)) {
+			String[] words = str.trim().split(" ");
+			System.out.println("Number of words in the string is: = " + words.length);
+		} else {
+			System.out.println("Invalid String!, Please enter the valid String.");
+		}
+
+		// Method: 2
+		int count = 1;
+
+		for (int i = 0; i < str.length() - 1; i++) {
+				if ((str.charAt(i) == ' ') && (str.charAt(i + 1) != ' ')) {
+					count++;
+				}
+			}
+		System.out.println("Number of words in a string = " + count);
+		sc.close();
+	}
+
+}
